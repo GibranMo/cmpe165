422b9d46826c4e9847fa9d709da17a3d59aa637e
GibranMo <Gibran@Gibrans-MacBook-Pro-677.local>
2015-05-07 02:38:08 -0700

    d

--- webproject/index2.php
+++ webproject/index2.php
@@ -1,24 +1,66 @@
+<?php session_start()?>
 
-<div class='movie_choice'>
-    Rate: Raiders of the Lost Ark
-    <div id="r1" class="rate_widget">
-        <div class="star_1 ratings_stars"></div>
-        <div class="star_2 ratings_stars"></div>
-        <div class="star_3 ratings_stars"></div>
-        <div class="star_4 ratings_stars"></div>
-        <div class="star_5 ratings_stars"></div>
-        <div class="total_votes">vote data</div>
-    </div>
+<!DOCTYPE HTML> 
+<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles/SearchBox.css">
+		<link rel="stylesheet" type="text/css" href="styles/Navbar.css">
+
+		<link href='http://fonts.googleapis.com/css?family=Mr+Dafoe' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Amaranth:700' rel='stylesheet' type='text/css'>
+	
+		<link rel="stylesheet" href="styles/slideshow.css" />
+		<script src="jquery.js"></script>
+		<script src="player.js"></script>
+	</head>
+
+	<body> 
+<div id="hmenu">
+<?php 
+	include "menu.php";
+	include('dbconnect.php');
+?>  
 </div>
- 
-<div class='movie_choice'>
-    Rate: The Hunt for Red October
-    <div id="r2" class="rate_widget">
-        <div class="star_1 ratings_stars"></div>
-        <div class="star_2 ratings_stars"></div>
-        <div class="star_3 ratings_stars"></div>
-        <div class="star_4 ratings_stars"></div>
-        <div class="star_5 ratings_stars"></div>
-        <div class="total_votes">vote data</div>
-    </div>
-</div>
+
+	
+	<table width="500" cellspacing = "10" >
+		<tbody>
+	<?php
+	$tbl_name = "Restaurant";
+	$sql = "SELECT name, iconimage, address, rating FROM $tbl_name";
+	$result2 = $conn->query($sql);
+		
+		if ($result2->num_rows > 0)
+		{
+			while ($row = $result2->fetch_assoc())
+			{
+		
+				echo "<tr>";
+				echo "<td>";
+				echo "<b>".$row["name"]. "</b>";
+				echo "<br>";
+				echo "<img src=\"{$row['iconimage']}\" height=\"100\" width=\"160\">";
+				echo "</td>";
+				
+				echo "<td>";
+				echo "<b>Address:</b>";
+				echo $row["address"];
+				echo "</td>";
+				
+				echo "<td>";
+				echo "<b>Rating:</b>";
+				echo $row["rating"];
+				echo "</td>";
+				echo "</tr>";
+	
+		}
+	}
+	
+	?>
+			</tbody>
+		</table>
+		
+	
+</body>
+</html>
